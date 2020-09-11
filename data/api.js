49e239ed606f8e8cbffe3c003c987e0d129eafe1
Agustin Carrasco <asermax@gmail.com>
2020-09-11 01:34:45 -0300

    6.5 - Include credentials on API calls

--- data/api.js
+++ data/api.js
@@ -3,14 +3,17 @@ import camelcaseKeys from 'camelcase-keys';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-const DEFAULT_HEADERS = {
-  'content-type': 'application/json',
-};
-
 const generateUrl = (path) => `${API_URL}/${path}`;
+const generateHeaders = () => ({
+  'content-type': 'application/json',
+  'x-csrftoken': cookie.get('csrftoken'),
+});
 
 export const get = async (path) => {
-  const response = await fetch(generateUrl(path));
+  const response = await fetch(generateUrl(path), {
+    credentials: 'include',
+    headers: generateHeaders(),
+  });
   const data = await response.json();
 
   return camelcaseKeys(data);
@@ -19,7 +22,8 @@ export const get = async (path) => {
 export const post = async (path, body) => {
   const response = await fetch(generateUrl(path), {
     method: 'POST',
-    headers: DEFAULT_HEADERS,
+    headers: generateHeaders(),
+    credentials: 'include',
     body: JSON.stringify(body),
   });
   const data = await response.json();
